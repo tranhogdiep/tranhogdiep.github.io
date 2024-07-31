9cf1b58cfaf48ac63bd4fd249ff98865db231b5c
tranhongdiep96.96@gmail.com <tranhongdiep96.96@gmail.com>
2024-07-31 18:34:00 +0700

    info popup concent

--- src/info.js
+++ src/info.js
@@ -1,47 +1,14 @@
 var popupDiv;
 
 export function ShowInfoPopup() {
-    if (popupDiv == null) {
-        CreatePopup();
+    if(popupDiv == null){
+        popupDiv= document.getElementById("infopopup")
+        popupDiv.addEventListener("click", (e) => {
+            if(e.target == popupDiv){
+            popupDiv.style.display = "none";
+            }
+        })
     }
-    popupDiv.style.display = "block";
-}
-function CreatePopup() {
-    // Create the outer div with the id "myPopup" and class "popup"
-    popupDiv = document.createElement('div');
-    popupDiv.id = 'myPopup';
-    popupDiv.classList.add('popup');
-    popupDiv.addEventListener("click", () => {
-        popupDiv.style.display = "none";
+    popupDiv.style.display = "flex";
 
-    })
-
-    // Create the inner div with the class "popup-content"
-    const popupContent = document.createElement('div');
-    popupContent.classList.add('popup-content');
-
-    // Create the close button with the class "close"
-    const closeButton = document.createElement('span');
-    closeButton.classList.add('close');
-    closeButton.textContent = '×';
-    closeButton.addEventListener("click", () => {
-        popupDiv.style.display = "none";
-
-    })
-
-    // Create the image element
-    const popupImage = document.createElement('img');
-    popupImage.src = 'assets/images/info.png';
-    popupImage.alt = 'Popup Image';
-    popupImage.classList.add("popupimage");
-
-
-    // Append the elements to the DOM
-    popupContent.appendChild(closeButton);
-    popupContent.appendChild(popupImage);
-
-    popupDiv.appendChild(popupContent);
-
-    // Append the popup to the body
-    document.body.appendChild(popupDiv);
 }
